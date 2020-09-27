1df84633254643b9eaedf2ee7d2d0dd372bf7ad3
Jaycee Barrameda <jmbarrameda@up.edu.ph>
2020-09-27 15:17:02 +0800

    Updates

---
+++ precache-manifest.fe892db287cb8df68e65e53237f2a8de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1148598f7ecd179d47f629a806b7b29",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "6d2311b432c27f6b2a0f",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "23ac3e7c0ca3cd24175c",
+    "url": "/facerecognitionbrain/static/css/main.fbdfc7ad.chunk.css"
+  },
+  {
+    "revision": "6d2311b432c27f6b2a0f",
+    "url": "/facerecognitionbrain/static/js/2.ac52a0c9.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionbrain/static/js/2.ac52a0c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ac3e7c0ca3cd24175c",
+    "url": "/facerecognitionbrain/static/js/main.1df5ef6a.chunk.js"
+  },
+  {
+    "revision": "a0d072c450e7e6f545a0",
+    "url": "/facerecognitionbrain/static/js/runtime-main.3b236050.js"
+  },
+  {
+    "revision": "58c1d842950c9aae78818c208b9d9be7",
+    "url": "/facerecognitionbrain/static/media/lightbulb.58c1d842.svg"
+  }
+]);
